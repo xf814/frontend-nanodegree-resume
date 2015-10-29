28d99b3bc8570999fa0f3af6c452deec3fff9616
xfxf820 <xfxf820@163.com>
2015-10-29 21:32:04 +0800

    Delete helloworld and add my formatted name and role.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,17 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- $("#main").append("Feng Xiong");
+ //$("#main").append("Feng Xiong");
+
+/*
+ var awesomeThoughts = "I am Feng Xiong and I am AWESOME!";
+ console.log(awesomeThoughts);
+ var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+ $("#main").append(funThoughts);
+ */
+var name = "Feng Xiong";
+var role = "Web Developer";
+var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
